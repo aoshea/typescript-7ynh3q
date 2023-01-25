2b995da14cd13d6b67cc78c70d26891788dbe8f0
aoshea <aoshea@users.noreply.github.com>
2023-01-25 18:21:56 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,76 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>Trie</h1>`;
+
+function TrieNode(key = '', value = '', children = [], count = 0) {
+  this.key = key;
+  this.value = value;
+  this.children = children;
+  this.count = count;
+}
+
+function Trie() {
+  this.root = new TrieNode();
+}
+
+Trie.prototype.insert = function (str: string) {
+  let curr = this.root;
+  for (const c of str) {
+    const index = curr.children.indexOf((x) => x.key === c);
+    if (index !== -1) {
+      curr = curr.children[index];
+    } else {
+      curr.children.push(new TrieNode(c));
+      curr = curr.children[curr.children.length - 1];
+    }
+  }
+  curr.count += 1;
+  curr.value = str;
+};
+
+Trie.prototype.contains = function (str: string): boolean {
+  let curr = this.root;
+  for (const c of str) {
+    const index = curr.children.findIndex((x) => x.key === c);
+    if (index !== -1) {
+      curr = curr.children[index];
+    } else {
+      return false;
+    }
+  }
+  return curr.count > 0;
+};
+
+Trie.prototype.startsWith = function (str: string): string[] {
+  let curr = this.root;
+  for (const c of str) {
+    const index = curr.children.findIndex((x) => x.key === c);
+    if (index !== -1) {
+      curr = curr.children[index];
+    } else {
+      return [];
+    }
+  }
+  const result = [];
+  const q = [curr];
+  while (q.length > 0) {
+    const node = q.pop();
+    for (const child of node.children) {
+      q.push(child);
+    }
+    if (node.count > 0) {
+      result.push(node.value);
+    }
+  }
+  return result;
+};
+
+// tests
+const trie = new Trie();
+console.log(trie.insert('premium'));
+console.log(trie.insert('pre'));
+console.log(trie.contains('premium'));
+console.log(trie.startsWith('pre'));
